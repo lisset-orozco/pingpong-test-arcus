af7eaff60ea762a9edf7d24d1fb3123a74eaedb8
lisset-orozco <liss612@gmail.com>
2019-03-07 10:49:47 -0600

    add validations to record_spec

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
+  subject { FactoryBot.create(:record) }
+  let(:attributes) { attributes_for(:record) }
 
   describe 'associations' do
     %i[user game].each { |association| it { should belong_to(association) } }
+  end
+
+  describe 'validations' do
+    it { attributes.each { |attribute| should validate_presence_of(attribute.first) } }
   end
 
   describe '.ranking_list' do
